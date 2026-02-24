49536b47f47b3672c82a5c871deb755a9301bf39
seria <seria.ati@gmail.com>
2026-02-24 17:19:37 +0900

    feat: Use enum for specialty & literals for rarity in zzz weapon

--- hb_data/zzz/models/weapon.py
+++ hb_data/zzz/models/weapon.py
@@ -1,4 +1,8 @@
+from typing import Literal
+
 from pydantic import BaseModel, Field
+
+from hb_data.zzz.models.enums import Specialty
 
 __all__ = ("Weapon",)
 
@@ -6,7 +10,7 @@ __all__ = ("Weapon",)
 class Weapon(BaseModel):
     id: int = Field(alias="ItemID")
     name: str = Field(alias="Name")
-    specialty: int = Field(alias="WeaponSpecialty")
+    specialty: Specialty = Field(alias="WeaponSpecialty")
     rarity: int = Field(alias="Rarity")
 
     @property
@@ -14,5 +18,5 @@ class Weapon(BaseModel):
         return f"https://zzz.honeyhunterworld.com/img/item/{self.id}-weapon_data_icon.webp"
 
     @property
-    def rarity_str(self) -> str:
-        return ("C", "B", "A", "S")[self.rarity - 1]
+    def rarity_str(self) -> Literal["B", "A", "S"]:
+        return ("C", "B", "A", "S")[self.rarity - 1]  # pyright: ignore[reportReturnType]
