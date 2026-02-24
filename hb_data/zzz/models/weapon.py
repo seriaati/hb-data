5edc61e8a080340401ec444089102aebf270cbec
seria <seria.ati@gmail.com>
2026-02-24 10:46:28 +0900

    feat: Add ZZZ w-engine icon

--- hb_data/zzz/models/weapon.py
+++ hb_data/zzz/models/weapon.py
@@ -8,3 +8,7 @@ __all__ = ("Weapon",)
 class Weapon(BaseModel):
     id: int = Field(alias="ItemID")
     name: str = Field(alias="Name")
+
+    @property
+    def icon(self) -> str:
+        return f"https://zzz.honeyhunterworld.com/img/item/{self.id}-weapon_data_icon_35.webp"
